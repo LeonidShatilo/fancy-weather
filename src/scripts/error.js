ae29e9a6b2b75691fa942e21bd6d8ce4ecbcd341
Leonid Shatilo <shatiloleonid@gmail.com>
2021-01-15 12:52:18 +0300

    feat: implement error display

---
+++ src/scripts/error.js
@@ -0,0 +1,18 @@
+const ERROR = document.querySelector('.error');
+const ERROR_MESSAGE = document.querySelector('.error__message');
+const ERROR_CONFIRM_BUTTON = document.querySelector('.error__confirm-button');
+
+export function showError(descriprion) {
+  ERROR_MESSAGE.innerHTML = descriprion;
+  ERROR.classList.add('error__visible');
+}
+
+ERROR_CONFIRM_BUTTON.addEventListener('click', () => {
+  ERROR.classList.add('error__hide');
+  setTimeout(() => {
+    ERROR.classList.remove('error__visible');
+  }, 400);
+  setTimeout(() => {
+    ERROR.classList.remove('error__hide');
+  }, 500);
+});
