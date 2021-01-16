9d58a64c2411ebbd4f5595eed7e51faebcf6b37b
Leonid Shatilo <shatiloleonid@gmail.com>
2021-01-17 00:05:00 +0300

    feat: add data module

---
+++ src/scripts/data.js
@@ -0,0 +1,18 @@
+export let allData = {
+  currentLanguage: 'en',
+  currentUnitOfTemperature: 'celsius',
+  coordinates: {
+    lat: '',
+    lng: '',
+  },
+  userCoordinates: {
+    lat: '',
+    lng: '',
+  },
+  temperatureToday: '',
+  temperatureNextThreeDays: [0, 0, 0],
+  place: '',
+  city: '',
+  country: '',
+  offset: '',
+};
