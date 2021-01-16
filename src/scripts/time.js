6520dc40590fb23b06a5e2b155a7e1e5c8948fe4
Leonid Shatilo <shatiloleonid@gmail.com>
2021-01-16 19:52:37 +0300

    feat: implement adding a title for the next three days

--- src/scripts/time.js
+++ src/scripts/time.js
@@ -1,11 +1,11 @@
 import { LANGUAGE } from './language.js';
-import { currentLanguage } from './header.js';
+import { allData } from './data.js';
 
 const DATE = document.querySelector('.title__date');
 const TIME = document.querySelector('.title__time');
 const FIRST_DAY = document.querySelector('.weather__title-first-day');
-const SECOND_DAY = document.querySelector('.weather__second-day');
-const THIRD_DAY = document.querySelector('.weather__third-day');
+const SECOND_DAY = document.querySelector('.weather__title-second-day');
+const THIRD_DAY = document.querySelector('.weather__title-third-day');
 
 function addZero(n) {
   return (parseInt(n, 10) < 10 ? '0' : '') + n;
@@ -21,14 +21,33 @@ function showTime() {
   setTimeout(showTime, 1000);
 }
 
-function showDate() {
+export function showDate() {
   let today = new Date();
   let dayWeek = today.getDay();
   let dayDate = today.getDate();
   let dayMonth = today.getMonth();
 
-  DATE.innerHTML = `${LANGUAGE.shortDayOfWeek[currentLanguage][dayWeek]},
-                    ${dayDate} ${LANGUAGE.month[currentLanguage][dayMonth]}`;
+  DATE.innerHTML = `${
+    LANGUAGE.shortDayOfWeek[allData.currentLanguage][dayWeek]
+  },
+  ${dayDate} ${LANGUAGE.month[allData.currentLanguage][dayMonth]}`;
+
+  today.setDate(today.getDate() + 1);
+  let firstDay = today.getDay();
+  today.setDate(today.getDate() + 1);
+  let secondDay = today.getDay();
+  today.setDate(today.getDate() + 1);
+  let thirdDay = today.getDay();
+
+  FIRST_DAY.innerHTML = `${
+    LANGUAGE.dayOfWeek[allData.currentLanguage][firstDay]
+  }`;
+  SECOND_DAY.innerHTML = `${
+    LANGUAGE.dayOfWeek[allData.currentLanguage][secondDay]
+  }`;
+  THIRD_DAY.innerHTML = `${
+    LANGUAGE.dayOfWeek[allData.currentLanguage][thirdDay]
+  }`;
 }
 
 showTime();
