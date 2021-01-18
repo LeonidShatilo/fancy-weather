757d90142a230b0a7bb51cffdb5b72f7ccf32315
Leonid Shatilo <shatiloleonid@gmail.com>
2021-01-18 23:46:20 +0300

    feat: add preloader module

---
+++ src/scripts/preloader.js
@@ -0,0 +1,23 @@
+import { LANGUAGE } from './language.js';
+import { allData } from './data.js';
+
+const PRELOADER = document.querySelector('.preloader');
+const PRELOADER_TEXT = document.querySelector('.preloader__text');
+
+export function addPreloaderText() {
+  return new Promise((resolve) => {
+    PRELOADER_TEXT.innerHTML = LANGUAGE.preloader[allData.currentLanguage];
+    setTimeout(() => {
+      resolve();
+    }, 0);
+  });
+}
+
+export function removePreloader() {
+  setTimeout(() => {
+    PRELOADER.classList.add('preloader--hide');
+  }, 400);
+  setTimeout(() => {
+    document.body.removeChild(document.body.children[1]);
+  }, 500);
+}
