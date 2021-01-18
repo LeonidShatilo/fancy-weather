d6a56a2b1571a2028888f25181d0454931fd9309
Leonid Shatilo <shatiloleonid@gmail.com>
2021-01-18 23:49:17 +0300

    feat: add new properties to a data object

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -2,17 +2,36 @@ export let allData = {
   currentLanguage: 'en',
   currentUnitOfTemperature: 'celsius',
   coordinates: {
-    lat: '',
-    lng: '',
+    lat: 0,
+    lng: 0,
   },
   userCoordinates: {
+    lat: 0,
+    lng: 0,
+  },
+  userCoordinatesText: {
     lat: '',
     lng: '',
   },
   temperatureToday: '',
+  temperatureTodayInFahrenheit: '',
   temperatureNextThreeDays: [0, 0, 0],
+  temperatureNextThreeDaysInFahrenheit: [0, 0, 0],
+  weatherIcon: {
+    today: '',
+    nextThreeDays: [0, 0, 0],
+  },
+  weather: '',
+  feelsLike: '',
+  humidity: '',
+  wind: '',
   place: '',
   city: '',
   country: '',
   offset: '',
+  date: {
+    year: 0,
+    month: 0,
+    day: 0,
+  },
 };
