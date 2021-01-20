664036539f4690f68c9edaa0c77f07da3d301077
Leonid Shatilo <shatiloleonid@gmail.com>
2021-01-21 00:24:19 +0300

    feat: implement getting a weather description

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -2,14 +2,14 @@ import { showError } from './error.js';
 import { LANGUAGE } from './language.js';
 import { allData } from './data.js';
 
+const WEATHER = document.querySelector('.weather__today-description');
+const WIND = document.querySelector('.weather__wind');
+const HUMIDITY = document.querySelector('.weather__humidity');
 export const TEMP_TODAY = document.querySelector('.weather__temp-today');
 export const TEMP_FIRST = document.querySelector('.weather__temp-first-day');
 export const TEMP_SECOND = document.querySelector('.weather__temp-second-day');
 export const TEMP_THIRD = document.querySelector('.weather__temp-third-day');
-const WEATHER = document.querySelector('.weather__today-description');
-const WIND = document.querySelector('.weather__wind');
-const FEELS_LIKE = document.querySelector('.weather__feels-like');
-const HUMIDITY = document.querySelector('.weather__humidity');
+export const FEELS_LIKE = document.querySelector('.weather__feels-like');
 export const TITLE_WIND = document.querySelector('.weather__title-wind');
 export const WIND_UNIT = document.querySelector('.weather__wind-unit');
 export const TITLE_FEELS_LIKE = document.querySelector(
@@ -36,9 +36,21 @@ function getTemperatureInFahrenheit() {
   allData.temperatureNextThreeDaysInFahrenheit[2] = Math.round(
     (allData.temperatureNextThreeDays[2] * 9) / 5 + 32
   );
+  allData.feelsLikeInFahrenheit = Math.round(
+    (Number(allData.feelsLike) * 9) / 5 + 32
+  );
 }
 
-function getDataForNextDays(data) {
+function getDataWeatherToday(data) {
+  allData.weather = data.list[0].weather[0].description;
+  allData.temperatureToday = Math.round(data.list[0].main.temp);
+  allData.feelsLike = Math.round(data.list[0].main.feels_like);
+  allData.wind = data.list[0].wind.speed.toFixed(1);
+  allData.humidity = Math.round(data.list[0].main.humidity);
+  allData.weatherIcon.today = data.list[0].weather[0].icon;
+}
+
+function getDataWeatherForNextDays(data) {
   let nextDay = 1;
   let indexDays = 0;
 
@@ -61,7 +73,7 @@ function getDataForNextDays(data) {
   }
 }
 
-function insertDataIntoHtml() {
+function insertWeatherData() {
   TEMP_TODAY.innerHTML = allData.temperatureToday;
   TEMP_FIRST.innerHTML = allData.temperatureNextThreeDays[0];
   TEMP_SECOND.innerHTML = allData.temperatureNextThreeDays[1];
@@ -84,18 +96,13 @@ export function getWeather(lat, lng) {
   const APP_ID = '1d82dbf3046ed45fdb18c16592d6f620';
   let URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&lang=${LANG}&units=metric&appid=${APP_ID}`;
 
-  return fetch(URL)
+  fetch(URL)
     .then((response) => response.json())
     .then((data) => {
-      allData.weather = data.list[0].weather[0].description;
-      allData.temperatureToday = Math.round(data.list[0].main.temp);
-      allData.feelsLike = Math.round(data.list[0].main.feels_like);
-      allData.wind = data.list[0].wind.speed.toFixed(1);
-      allData.humidity = Math.round(data.list[0].main.humidity);
-      allData.weatherIcon.today = data.list[0].weather[0].icon;
-      getDataForNextDays(data);
+      getDataWeatherToday(data);
+      getDataWeatherForNextDays(data);
       getTemperatureInFahrenheit();
-      insertDataIntoHtml();
+      insertWeatherData();
       addIcons();
 
       return 'ok';
@@ -103,6 +110,22 @@ export function getWeather(lat, lng) {
     .catch((e) => {
       showError(LANGUAGE.error.weather[allData.currentLanguage]);
 
+      return;
+    });
+}
+
+export function getWeatherDescription(lat, lng) {
+  const LANG = allData.currentLanguage;
+  const APP_ID = '1d82dbf3046ed45fdb18c16592d6f620';
+  let URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&lang=${LANG}&units=metric&appid=${APP_ID}`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      allData.weather = data.list[0].weather[0].description;
+      WEATHER.innerHTML = allData.weather;
+    })
+    .catch((e) => {
       return;
     });
 }
