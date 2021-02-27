2f6baf59ee042d16f5ef98685a855afc7a87ce16
Leonid Shatilo <shatiloleonid@gmail.com>
2021-02-28 01:48:35 +0300

    refactor: change the behavior of the preloader

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -2,6 +2,7 @@ import { LANGUAGE } from './language.js';
 import { allData } from './data.js';
 
 const PRELOADER = document.querySelector('.preloader');
+const PRELOADER_ICON = document.querySelector('.preloader__icon');
 const PRELOADER_TEXT = document.querySelector('.preloader__text');
 
 export function addPreloaderText() {
@@ -14,15 +15,17 @@ export function addPreloaderText() {
 }
 
 export function removePreloader() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      PRELOADER.classList.add('preloader--hide');
-    }, 1000);
-    setTimeout(() => {
-      document.body.removeChild(document.body.children[1]);
-    }, 1100);
-    setTimeout(() => {
-      resolve();
-    }, 1200);
-  });
+  setTimeout(() => {
+    PRELOADER_ICON.classList.add('preloader__icon--hide');
+    PRELOADER_TEXT.classList.add('preloader__text--hide');
+  }, 700);
+  setTimeout(() => {
+    PRELOADER.classList.add('preloader--hide');
+  }, 1600);
+  setTimeout(() => {
+    document.body.removeChild(document.body.children[1]);
+  }, 2400);
+  setTimeout(() => {
+    document.body.classList.remove('no-scroll');
+  }, 2500);
 }
